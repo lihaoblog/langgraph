f4f2122cb3de84bd332c2b90f78cdd138eb3272c
lihao <lihao960603@outlook.com>
2025-10-20 17:28:46 +0800

    langgraph框架模式

---
+++ src/agent/graph.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+
+# 本地私有化部署的大模型
+llm = ChatOpenAI(
+    model='qwen3-8b',
+    temperature=0.8,
+    api_key='xx',
+    base_url="http://localhost:6006/v1",
+    extra_body={'chat_template_kwargs': {'enable_thinking': False}},
+)
+
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+graph = create_react_agent(
+    llm,
+    tools=[get_weather],
+    prompt="You are a helpful assistant"
+)
